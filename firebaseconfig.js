d8345dc9a82c63668dd1cdffe1e675fd27c98bc9
manzanotes123-wq <manzanotes123@gmail.com>
2025-08-06 23:29:01 -0600

    Update firebaseconfig.js

--- firebaseconfig.js
+++ firebaseconfig.js
@@ -1,5 +1,8 @@
 // firebaseconfig.js
-export const firebaseConfig = {
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
   apiKey: "AIzaSyBF_EEIamEevhQfgvkPbrz9wEQwqcH-pN0",
   authDomain: "votacion-f51c8.firebaseapp.com",
   projectId: "votacion-f51c8",
@@ -7,3 +10,8 @@ export const firebaseConfig = {
   messagingSenderId: "195415317932",
   appId: "1:195415317932:web:12dbe2fefc94fc6a921d9c"
 };
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export { db };
